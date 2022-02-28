eac3bd04520a6c2696132fd236e0def9f4f833d2
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-02-28 23:49:10 +0100

    Update side1.php

--- side1.php
+++ side1.php
@@ -10,17 +10,27 @@
     require_once '/home/mir/lib/db.php';
 
 //CHRIS - bygget videre på
-    for ($i=1; $i < 6; $i++) {
-      $oplæg = get_post($i);
-      $pids = get_post($i);
-      $content = get_post($i);
-      $date = get_post($i);
+for ($i=1; $i < 6; $i++) {
+$oplæg = get_post($i);
+$pids = get_post($i);
+$content = get_post($i);
+$date = get_post($i);
+$forfatter = get_post($i);
 
-      echo "<tr>";
-      echo "<br><td> ID er: ", $pids['pid'], "</td>", '<br> ';
-      echo "<td> Titlen er: ", $oplæg['title'],"</td>", '<br>';
-      echo "<td> Indholdet er: ", $content['content'],"</td>", '<br>';
-      echo "<td> Dato: ", $date['date'],"</td>", '<br>';
+// fornavn og efternavn virker ikke endnu
+$fornavn = get_user($i);
+$efternavn = get_user($i);
+
+echo "<tr>";
+echo "<br><td> Indlægs id: ", $pids['pid'], "</td>";
+echo "<br><td> Indlægs forfatter : ", $forfatter['uid'], "</td>", '<br> ';
+
+// virker ikke endnu med at få fornavn og efternavn på forfatteren endnu
+echo "<td> Fornavn og efternavn: ", $fornavn['firstname'], " ", $efternavn['lastname'], "</td>", '<br>';
+
+echo "<td> Title: ", $oplæg['title'],"</td>", '<br>';
+echo "<td> Indhold: ", $content['content'],"</td>", '<br>';
+echo "<td> Dato: ", $date['date'],"</td>", '<br>';
 
 /* LAURA
     $postID = get_pids();
