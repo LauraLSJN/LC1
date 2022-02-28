84a0084485282691c14c1ece1197e54f12fe9374
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-02-28 23:31:34 +0100

    Prøv at kør den.

--- side1.php
+++ side1.php
@@ -9,6 +9,20 @@
 <?php
     require_once '/home/mir/lib/db.php';
 
+//CHRIS - bygget videre på
+    for ($i=1; $i < 6; $i++) {
+      $oplæg = get_post($i);
+      $pids = get_post($i);
+      $content = get_post($i);
+      $date = get_post($i);
+
+      echo "<tr>";
+      echo "<br><td> ID er: ", $pids['pid'], "</td>", '<br> ';
+      echo "<td> Titlen er: ", $oplæg['title'],"</td>", '<br>';
+      echo "<td> Indholdet er: ", $content['content'],"</td>", '<br>';
+        echo "<td> Dato: ", $date['date'],"</td>", '<br>';
+
+/* LAURA
     $postID = get_pids();
     foreach ($postID as $postid){
       echo ' id: ', $postid;
@@ -19,6 +33,8 @@
       echo 'Titlen er: ', $oplæg['title'], '<br> </br>';
       echo 'Indholdet: ', $oplæg['content'];
     }
+
+    */
 
 ?>
   </body>
