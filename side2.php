bb91f032a82b5b7c49733cd75706e53961716e70
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-01 16:37:10 +0100

    Update side2.php

---
+++ side2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>side 2</title>
+  </head>
+  <body>
+    <?php  require_once '/home/mir/lib/db.php';
+
+    ?>
+
+  </body>
+</html>
