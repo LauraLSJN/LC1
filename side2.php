8cc2939c1ee06ca91d8f19c7942219da65a0e406
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-01 21:34:58 +0100

    Update side2.php

--- side2.php
+++ side2.php
@@ -15,19 +15,21 @@
 
     <?php  require_once '/home/mir/lib/db.php';
 
-    //Printer en liste ud med alle brugere (uids) i databasen, organiseret i punktform
+//Printer en liste ud med alle brugere (uids) i databasen, organiseret i punktform
   $getUids = get_uids();
   echo "<br> Her kan du se listen af uids: <br>";
   foreach ($getUids as $uid){
     echo ' <li> ', $uid, "<br>";
   }
 
+//Hvis der bliver skrevet til tal, vil der kommme en meddelelse op
   echo "<br>";
   if (is_numeric($_GET['id'])) {
   echo "bruger uid er ikke et tal, prøv igen";
   exit;
   }
 
+// Hvis der mangler et bruger uid, vil der komme en meddelelse op
   if (empty($_GET['id'])) {
     echo "Parameteren bruger uid mangler ";
     exit;
