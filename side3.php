ae5699d8f47083a433150efc8f2eb611d4fd595e
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-01 16:36:42 +0100

    nye sider til punkt 2 og 3.

---
+++ side3.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>side 3</title>
+  </head>
+  <body>
+    <?php require_once '/home/mir/lib/db.php';
+
+     ?>
+  </body>
+</html>
