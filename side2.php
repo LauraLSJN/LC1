cee97ce8199dd119fac9af2131d9e263a2488c27
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-01 17:35:34 +0100

    Update side2.php

--- side2.php
+++ side2.php
@@ -5,8 +5,22 @@
     <title>side 2</title>
   </head>
   <body>
+
+    <form action="side2.php" method="get">
+  <label for="id">  Angiv et id for en bruger (uid):
+    <input type="text" id="id" name="id">
+    <input type="submit">
+  </form>
+  <br>
+
     <?php  require_once '/home/mir/lib/db.php';
 
+    $uid = $_GET["id"];
+    $title = get_post($uid);
+
+//title virker ikke endnu
+        echo "<br>Bruger: ", $_GET["id"], '<br>';
+        echo "<br> Titel: ", $title['title'];
     ?>
   </body>
 </html>
