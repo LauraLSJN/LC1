56f01e8004001ac2fa7c0db1984be644ae49dc3b
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-01 18:04:02 +0100

    Update side3.php

--- side3.php
+++ side3.php
@@ -7,6 +7,13 @@
   <body>
     <?php require_once '/home/mir/lib/db.php';
 
+    //Printer en liste ud med alle (uids) brugere i databasen, organiseret i punktform
+$uids = get_uids();
+echo "Her kan du se listen af uids: <br>";
+foreach ($uids as $uid){
+  echo ' <li> ', $uid, "<br>";
+}
+
      ?>
   </body>
 </html>
