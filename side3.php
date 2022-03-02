e7de03309ca75c11c05d0c4eb1a28b83136870a5
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-02 11:37:18 +0100

    Update side3.php

--- side3.php
+++ side3.php
@@ -8,13 +8,12 @@
     <?php require_once '/home/mir/lib/db.php';
 
     //Printer en liste ud med alle (uids) brugere i databasen, organiseret i punktform
-  $getUids = get_uids();
-  echo "Der er ", count($getUids), " bruger id (uids) <br>"; //count funktionen returnere længden af arrayet der ligger i variablen $getUids
-  echo "<br>Her kan du se listen af brugere (uids): <br>";
-  foreach ($getUids as $uid){
-    echo " <li>  ", $uid, "<br>";
+    $getUids = get_uids();
+    echo "Der er ", count($getUids), " bruger id (uids) <br>"; //count funktionen returnere længden af arrayet der ligger i variablen $getUids
+    echo "<br>Her kan du se listen af brugere (uids): <br>";
+    foreach ($getUids as $uid){
+    echo "<li> <a href='side2.php?id=".$uid."'>".$uid."</a> </li> <br>";
   }
-
      ?>
   </body>
 </html>
