ab2437067c65ebec160065f989736ee84524ceb5
CWulffeld <35725082+CWulffeld@users.noreply.github.com>
2022-03-09 12:11:02 +0100

    Update side4.php

--- side4.php
+++ side4.php
@@ -4,26 +4,42 @@
     <meta charset="utf-8">
     <title>Side 4</title>
   </head>
-      <!-- Christine Van Wulffeld (cvan) og Laura Juel Nielsen (lsjn) -->
-  <body>
+    <!-- Christine Van Wulffeld (cvan) og Laura Juel Nielsen (lsjn) -->
+<body>
 
+    <form  action="side4.php" method="post">
+      <label for="uid"> <b> Uid: </b> </label>
+<br>
+    <input type="text" name="uid">
+<br>
 
-    <form  action="side4.php" method="get">
       <label for="title"> <b> Title: </b> </label>
-      <br>
+<br>
          <input  type="text" id="title" name="title">
 <br>
          <label for="indhold"> <b> Indhold: </b> </label>
-         <br>
+<br>
          <textarea id="indhold" name="indhold" rows="25" cols="100">
        </textarea>
-         <br>
-        <input type="submit">
+<br>
+        <input type="submit" name="submit">
     </form>
 
 
-
     <?php require_once '/home/mir/lib/db.php';
+    //Tilføjer brugerinput til en variabel i PHP koden
+    $uid = $_POST['uid'];
+    $title = $_POST['title'];
+    $indhold = $_POST['indhold'];
+
+
+
+    //Udfører metoden add_post med de indtastede værdier i uid, title og indhold
+$tilføjPost = add_post($uid, $title, $indhold);
+
+
+    echo $tilføjPost($uid,$title,$indhold);
+
 
      ?>
 
